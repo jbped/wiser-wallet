cfbae1491d016d1ae9d3c83e12b2392e9388e0b0
Jake P <76881086+jbped@users.noreply.github.com>
2021-08-30 20:38:13 -0600

    idb and tweaks to manifest and service-worker

---
+++ public/js/idb.js
@@ -0,0 +1,74 @@
+let db;
+
+// Open connection with IndexedDb Budget database version 1
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    // Create new table called new_transaction
+    db.createObjectStore("new_transaction", { autoIncrement: true });
+}
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
+}
+
+function saveRecord(record) {
+    // open new transaction with db with read and write perms
+    const transaction = db.transaction(["new_transaction"], "readwrite");
+
+    // access object store
+    const transactionObjStore = transaction.objectStore("new_transaction");
+
+    // Add new record to transcation Obj Store
+    transactionObjStore.add(record)
+}
+
+
+function uploadTransaction() {
+    const transaction = db.transaction(["new_transaction"], "readwrite");
+
+    const transactionObjStore = transaction.objectStore("new_transaction");
+
+    const getAll = transactionObjStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(["new_transaction"], "readwrite");
+                    // access the new_transaction object store
+                    const transactionObjStore = transaction.objectStore("new_transaction");
+                    // clear all items in store
+                    transactionObjStore.clear();
+
+                    alert("All saved transactions has been submitted!");
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+window.addEventListener("online", uploadTransaction)
--- public/service-worker.js
+++ public/service-worker.js
@@ -4,18 +4,19 @@ const CACHE_NAME = APP_PREFIX + VERSION
 const DATA_CACHE_NAME = "Data-" + APP_PREFIX + VERSION
 
 const FILES_TO_CACHE = [
+    "/",
     "./index.html",
     "./manifest.json",
-    "./assets/css/style.css",
-    './assets/icons/icon-72x72.png',
-    './assets/icons/icon-96x96.png',
-    './assets/icons/icon-128x128.png',
-    './assets/icons/icon-144x144.png',
-    './assets/icons/icon-152x152.png',
-    './assets/icons/icon-192x192.png',
-    './assets/icons/icon-384x384.png',
-    './assets/icons/icon-512x512.png',
-    "./assets/js/index.js"
+    "./css/styles.css",
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png',
+    "./js/index.js"
 ];
 
 // Install Service Worker
@@ -28,16 +29,75 @@ self.addEventListener("install", function (e) {
     )
 });
 
-// Activate Service Worker
-self.addEventListener("activate", function (e) {
+// // Activate Service Worker
+// self.addEventListener("activate", function (e) {
+//     e.waitUntil(
+//         caches.keys().then(function (keyList) {
+//             let cacheKeepList = keyList.filter(function (key) {
+//                 return key.indexOf(APP_PREFIX)
+//             })
+//             cacheKeepList.push(CACHE_NAME)
+
+//             return Promise.all(keyList.map(function (key, i) {
+//                 if (cacheKeepList.indexOf(key) === -1) {
+//                     console.log(`Deleting cache: ${keyList[i]}`);
+//                     return caches.delete(keyList[i])
+//                 }
+//             }))
+//         })
+//     )
+// });
+
+// // Fetch Interrupt With Service Worker
+// self.addEventListener("fetch", function (e) {
+//     // if requested url includes /api (api fetch request)
+//     if (e.request.url.includes("/api")) {
+//         console.log(caches)
+//         // Intercept and respond with
+//         e.respondWith(
+//             caches
+//                 // open the api cache
+//                 .open(DATA_CACHE_NAME)
+//                 .then(cache => {
+//                     console.log(cache)
+//                     return fetch(e.request)
+//                         .then(response => {
+//                             // Good Fetch response, clone response and put it in the cache object, the key is the url
+//                             if (response.status === 200) {
+//                                 cache.put(e.request.url, response.clone())
+//                             }
+//                             return response;
+//                         })
+//                         .catch(err => {
+//                             return cache.match(e.request);
+//                         });
+//                 })
+//                 .catch(err => console.log(err))
+//         );
+//         return;
+//     }
+//     e.respondWith(
+//         fetch(e.request).catch(function () {
+//             return caches.match(e.request).then(function (response) {
+//                 if (response) {
+//                     return response;
+//                 } else if (e.request.headers.get("accept").includes("text/html")) {
+//                     return caches.match("/");
+//                 }
+//             });
+//         })
+//     );
+// })
+
+self.addEventListener("activate", function(e){
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeepList = keyList.filter(function (key) {
+        caches.keys().then(function (keyList){
+            let cacheKeepList = keyList.filter(function(key) {
                 return key.indexOf(APP_PREFIX)
             })
             cacheKeepList.push(CACHE_NAME)
 
-            return Promise.all(keyList.map(function (key, i) {
+            return Promise.all(keyList.map(function(key, i) {
                 if (cacheKeepList.indexOf(key) === -1) {
                     console.log(`Deleting cache: ${keyList[i]}`);
                     return caches.delete(keyList[i])
@@ -47,43 +107,18 @@ self.addEventListener("activate", function (e) {
     )
 });
 
-// Fetch Interrupt With Service Worker
-self.addEventListener("fetch", function (e) {
-    // if requested url includes /api (api fetch request)
-    if (e.request.url.includes("/api")) {
-        console.log(caches)
-        // Intercept and respond with
-        e.respondWith(
-            caches
-                // open the api cache
-                .open(DATA_CACHE_NAME)
-                .then(cache => {
-                    console.log(cache)
-                    return fetch(e.request)
-                        .then(response => {
-                            // Good Fetch response, clone response and put it in the cache object, the key is the url
-                            if (response.status === 200) {
-                                cache.put(e.request.url, response.clone())
-                            }
-                            return response;
-                        })
-                        .catch(err => {
-                            return cache.match(e.request);
-                        });
-                })
-                .catch(err => console.log(err))
-        );
-        return;
-    }
+self.addEventListener("fetch", function(e) {
+    console.log(`Fetch request: ${e.request.url}`)
     e.respondWith(
-        fetch(e.request).catch(function () {
-            return caches.match(e.request).then(function (response) {
-                if (response) {
-                    return response;
-                } else if (e.request.headers.get("accept").includes("text/html")) {
-                    return caches.match("/");
-                }
-            });
+        caches.match(e.request).then(function(request) {
+            if(request) {
+                console.log(`Responding with cache: ${e.request.url}`)
+                return request;
+            } else {
+                console.log(`File is not cached, fetching: ${e.request.url}`);
+                return fetch(e.request)
+            }
+            
         })
-    );
+    )
 })
