195a1544a2d063511f8aa6128e9a442e3439ad77
Jake P <76881086+jbped@users.noreply.github.com>
2021-09-05 16:05:32 -0600

    corrected service worker function

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,6 @@
 const APP_PREFIX = "Wiser_Wallet-";
 const VERSION = "version_01";
-const CACHE_NAME = APP_PREFIX + VERSION
-// const DATA_CACHE_NAME = "Data-" + APP_PREFIX + VERSION
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   "/",
@@ -116,7 +115,21 @@ self.addEventListener('install', function (e) {
       return cache.addAll(FILES_TO_CACHE)
     })
   )
-})
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // console.log(`Responding with cache: ${e.request.url}`)
+        return request;
+      } else {
+        // console.log(`File is not cached, fetching: ${e.request.url}`);
+        return fetch(e.request)
+      }
+    })
+  );
+});
 
 self.addEventListener("activate", function (e) {
   e.waitUntil(
@@ -136,39 +149,3 @@ self.addEventListener("activate", function (e) {
   )
 });
 
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        // console.log(`Responding with cache: ${e.request.url}`)
-        return request;
-      } else {
-        // console.log(`File is not cached, fetching: ${e.request.url}`);
-        return fetch(e.request)
-      }
-
-    })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create keeplist
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      })
-      // add current cache name to keeplist
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          // console.log('deleting cache : ' + keyList[i]);
-          return caches.delete(keyList[i]);
-        }
-      }));
-    })
-  );
-});
